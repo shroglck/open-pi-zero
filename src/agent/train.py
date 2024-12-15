overfit to small data, fix causal masking to igore unused tokens, use gamma schedule for flow by default

src/agent/train.py
@@ -6,6 +6,7 @@ Main training agent
 import logging
 import os
 import random
+from collections import deque
 
 import bitsandbytes as bnb
 import einops
@@ -38,6 +39,7 @@ class TrainAgent:
         self.num_gpus = torch.cuda.device_count()
         self.gpu_id = int(cfg.gpu_id)
         self.multi_gpu = self.num_gpus > 1
+        self.main_rank = self.gpu_id == 0
 
         # training params
         self.n_epochs = cfg.n_epochs
@@ -49,7 +51,7 @@ class TrainAgent:
         self.checkpoint_dir = os.path.join(self.logdir, "checkpoint")
         os.makedirs(self.checkpoint_dir, exist_ok=True)
         self.use_wandb = cfg.get("wandb", None)
-        if self.use_wandb and self.gpu_id == 0:
+        if self.use_wandb and self.main_rank:
             wandb.init(
                 entity=cfg.wandb.entity,
                 project=cfg.wandb.project,
@@ -79,6 +81,7 @@ class TrainAgent:
             model = self.model
         if cfg.load_pretrained_weights:
             model.load_pretrained_weights()
+            model.freeze_embedding()
         if self.multi_gpu:
             dist.barrier()
         log_allocated_gpu_memory(log, "loading model")
@@ -92,11 +95,14 @@ class TrainAgent:
         self.tokenizer = AutoTokenizer.from_pretrained(
             cfg.pretrained_model_path, padding_side="right"
         )
-        assert self.tokenizer.padding_side == "right"
 
         # processor
-        num_image_tokens = cfg.vision.config.num_image_tokens
-        self.processor = VLAProcessor(self.tokenizer, num_image_tokens, cfg.max_seq_len)
+        self.processor = VLAProcessor(
+            self.tokenizer,
+            num_image_tokens=cfg.vision.config.num_image_tokens,
+            max_seq_len=cfg.max_seq_len,
+            tokenizer_padding=cfg.tokenizer_padding,
+        )
 
         # dataloader --- use the same for all ranks
         dataset_wrapper = TorchRLDSInterleavedDataset(
@@ -106,29 +112,29 @@ class TrainAgent:
         self.dataloader = DataLoader(
             dataset_wrapper.dataset,
             batch_size=cfg.per_device_batch_size,
-            num_workers=0,  # important to keep this to 0 so PyTorch does not mess with the parallelism
+            num_workers=0,  # let TFDS handle parallelism
             shuffle=False,
             pin_memory=True,
             # sampler=DistributedSampler(dataset_wrapper.dataset),
-        )
-        num_batch_per_epoch = (
-            len(dataset_wrapper.dataset) // cfg.per_device_batch_size // self.num_gpus
-        )
+        )  # full bridge dataset has 2195527 transitions and 60064 trajectories
+        num_batch_per_epoch = len(dataset_wrapper.dataset) // cfg.global_batch_size
         log.info(f"Global batch size: {cfg.global_batch_size}")
         log.info(f"Per device batch size: {cfg.per_device_batch_size}")
         log.info(f"Gradient accumulation steps: {self.grad_accumulation_steps}")
         log.info(f"Number of batches per epoch: {num_batch_per_epoch}")
 
         # optimizer
-        self.trained_parameters = (
-            # list(mode.embed_tokens.parameters())
-            list(model.vision_tower.parameters())
-            + list(model.multi_modal_projector.parameters())
-            + list(model.joint_model.parameters())
-            + list(model.action_time_encoder.parameters())
-            + list(model.proprio_encoder.parameters())
-            + list(model.action_decoder.parameters())
-        )
+        if cfg.train_action_only:  # 0.315B
+            self.trained_parameters = model.action_expert_parameters
+        else:
+            self.trained_parameters = (
+                list(model.vision_tower.parameters())
+                + list(model.multi_modal_projector.parameters())
+                + list(model.joint_model.parameters())
+                + list(model.action_time_encoder.parameters())
+                + list(model.proprio_encoder.parameters())
+                + list(model.action_decoder.parameters())
+            )
         self.optimizer = bnb.optim.AdamW8bit(
             self.trained_parameters,
             lr=cfg.learning_rate,
@@ -160,6 +166,7 @@ class TrainAgent:
         epoch = 1
         cnt_batch = 0
         cnt_update = 0
+        loss_train_deque = deque(maxlen=self.grad_accumulation_steps)
         if self.multi_gpu:
             import torch.distributed as dist
 
@@ -169,14 +176,13 @@ class TrainAgent:
                 cnt_update_in_epoch = 0
                 """
                 batch: dict with keys 'observation', 'task', 'action', 'dataset_name', 'action_pad_mask'
-                observation: 'image_primary' (torch.Size([3, 1, 256, 256, 3], uint8), 'image_wrist', 'timestep' (torch.Size([3, 1])), 'pad_mask_dict', 'timestep_pad_mask', 'task_completed' (torch.Size([3, 1, 4]), 'proprio' (torch.Size([3, 1, 7])
-                task: 'language_instruction', 'pad_mask_dict', 'image_primary', 'image_wrist', 'timestep' (torch.Size([3]))
-                action (torch.Size([3, 1, 4, 7], float32)
+                observation: 'image_primary' (torch.Size([bsz, 1, H, W, 3], uint8), 'image_wrist', 'timestep' (torch.Size([bsz, 1])), 'pad_mask_dict', 'timestep_pad_mask', 'task_completed' (torch.Size([bsz, window, 4]), 'proprio' (torch.Size([bsz, window, proprio_dim])
+                task: 'language_instruction', 'pad_mask_dict', 'image_primary', 'image_wrist', 'timestep' (torch.Size([bsz]))
+                action (torch.Size([bsz, window, horizon, action_dim], float32)
                 dataset_name
-                action_pad_mask (torch.Size([3, 1, 4, 7]))
+                action_pad_mask (torch.Size([bsz, window, horizon, action_dim]))
                 """
 
-                # extract and debug
                 with torch.autocast("cuda", dtype=torch.bfloat16, enabled=True):
                     images = batch["observation"]["image_primary"]
                     proprios = batch["observation"]["proprio"]
@@ -220,8 +226,6 @@ class TrainAgent:
                         model = self.model.module
                     else:
                         model = self.model
-                    if self.debug:
-                        log_allocated_gpu_memory(log, f"pre batch {batch_ind}")
                     loss_train = model.loss(
                         pixel_values=pixel_values.to(self.device),
                         input_ids=input_ids.to(self.device),
@@ -229,6 +233,7 @@ class TrainAgent:
                         actions=actions.to(self.device),
                         attention_mask=attention_mask.to(self.device),
                     )
+                    loss_train_deque.append(loss_train.item())
                 if self.debug:
                     log_allocated_gpu_memory(log, f"forward batch {batch_ind}")
 
@@ -243,9 +248,7 @@ class TrainAgent:
                         max_norm=self.max_grad_norm,
                     )
                     self.optimizer.step()
-                    # lr schedule
-                    if self.gpu_id == 0:
-                        self.lr_scheduler.step()
+                    self.lr_scheduler.step()
                     if self.debug:
                         log_allocated_gpu_memory(
                             log, f"optimizer step batch {batch_ind}"
@@ -262,13 +265,14 @@ class TrainAgent:
                 loss_val = None
 
                 # log loss
-                if self.gpu_id == 0 and cnt_batch % self.log_freq == 0:
+                if self.main_rank and cnt_batch % self.log_freq == 0:
+                    loss_train_metric = np.mean(loss_train_deque)
                     log.info(
-                        f"Epoch {epoch} Batch {batch_ind}: train loss {loss_train:8.4f} | t:{timer():8.4f}"
+                        f"Epoch {epoch} Batch {batch_ind}: train loss {loss_train_metric:8.4f} | lr: {self.optimizer.param_groups[0]['lr']:8.6f} | t:{timer():8.4f}"
                     )
-                    if self.use_wandb and self.gpu_id == 0:
+                    if self.use_wandb:
                         wandb_metrics = {
-                            "loss - train": loss_train,
+                            "loss - train": loss_train_metric,
                             "gradient steps": cnt_update,
                             "lr": self.optimizer.param_groups[0]["lr"],
                         }
@@ -280,11 +284,11 @@ class TrainAgent:
                 cnt_batch += 1
 
                 # save model at batch level
-                if self.gpu_id == 0 and cnt_batch % self.save_model_batch_freq == 0:
+                if self.main_rank and cnt_batch % self.save_model_batch_freq == 0:
                     self.save_model(epoch)
 
             # Save model at epoch level --- always save at the end
-            if self.gpu_id == 0 and (
+            if self.main_rank and (
                 epoch % self.save_model_epoch_freq == 0 or epoch == self.n_epochs
             ):
                 self.save_model(epoch)

src/model/vla/model.py
@@ -44,7 +44,7 @@ class VLA(nn.Module):
         self.gamma_alpha = cfg.get("flow_gamma_alpha", 1.5)
         self.gamma_beta = cfg.get("flow_gamma_beta", 1)
         self.gamma_max = 1 - self.sig_min
-        self.schedule = cfg.get("flow_schedule", "linear")
+        self.schedule = cfg.get("flow_schedule", "gamma")
         assert self.schedule in [
             "linear",
             "gamma",
@@ -55,8 +55,7 @@ class VLA(nn.Module):
             cfg.vocab_size,
             self.image_text_hidden_size,
             self.pad_token_id,
-        )
-        self.embed_tokens.weight.requires_grad = False
+        )  # 0.527B parameters
 
         # Vision
         self.vision_tower = hydra.utils.instantiate(cfg.vision)
@@ -88,6 +87,28 @@ class VLA(nn.Module):
                 self.vocab_size,
                 bias=False,
             )
+
+    @property
+    def action_expert_parameters(self):
+        action_expert_parameters = []
+        for name, param in self.joint_model.named_parameters():
+            if (
+                "q_projs.2" in name
+                or "k_projs.2" in name
+                or "v_projs.2" in name
+                or "o_projs.2" in name
+                or "mlp.2" in name
+                or "layernorms.2" in name
+                or "layernorm.2" in name
+                or "action_norm" in name
+            ):
+                action_expert_parameters.append(param)
+        return (
+            list(self.action_time_encoder.parameters())
+            + list(self.action_decoder.parameters())
+            + list(self.proprio_encoder.parameters())
+            + action_expert_parameters
+        )
 
     @log_execution_time()
     def load_pretrained_weights(self):
@@ -161,6 +182,9 @@ class VLA(nn.Module):
         self.joint_model.load_state_dict(joint_model_state_dict, strict=False)
         print("Loaded pre-trained weights for lm part of the joint model")
 
+    def freeze_embedding(self):
+        self.embed_tokens.weight.requires_grad = False
+
     def tie_weights(self):
         self.lm_head.weight = self.embed_tokens.weight
 
@@ -173,7 +197,7 @@ class VLA(nn.Module):
         # shape: (Batch_Size, Seq_Len, Hidden_Size)
         inputs_embeds = self.embed_tokens(input_ids)
 
-        # TODO: cache this
+        # TODO: cache this in action inference
         # Extract image features from siglip and projector
         # [Batch_Size, Channels, Height, Width] -> [Batch_Size, Num_Patches, Embed_Dim]
         selected_image_feature = self.vision_tower(pixel_values.to(inputs_embeds.dtype))
@@ -226,6 +250,9 @@ class VLA(nn.Module):
         total_len = (
             max_num_image_text_tokens + self.num_proprio_tokens + self.num_action_tokens
         )
+        proprio_start = max_num_image_text_tokens
+        proprio_end = max_num_image_text_tokens + self.num_proprio_tokens
+        action_start = proprio_end
 
         # block attention
         causal_mask = torch.full(
@@ -234,18 +261,18 @@ class VLA(nn.Module):
             dtype=torch.float32,
             device=device,
         )  # smallest value, avoid using inf for softmax nan issues with padding
-
-        # image/text attend to itself
         for idx, num in enumerate(num_image_text_tokens):
-            causal_mask[idx, :num, :num] = 0
-
-        proprio_start = max_num_image_text_tokens
-        proprio_end = max_num_image_text_tokens + self.num_proprio_tokens
+            causal_mask[idx, :num, :num] = 0  # image/text attend to itself
+            causal_mask[
+                idx, proprio_start:proprio_end, :num
+            ] = 0  # proprio attend to image/text
+            causal_mask[idx, action_start:, :num] = 0  # action attend to image/text
         causal_mask[
-            :, proprio_start:proprio_end, :proprio_end
-        ] = 0  # proprio attend to itself and image/text
-        action_start = proprio_end
-        causal_mask[:, action_start:, :] = 0  # action attend to itself and all
+            :, proprio_start:proprio_end, proprio_start:proprio_end
+        ] = 0  # proprio attend to itself
+        causal_mask[
+            :, action_start:, proprio_start:
+        ] = 0  # action attend to itself and proprio
 
         # Add the head dimension
         # [Batch_Size, Q_Len, KV_Len] -> [Batch_Size, Num_Heads_Q, Q_Len, KV_Len]
@@ -584,7 +611,7 @@ if __name__ == "__main__":
     elif args.loss_only:
         dummy_actions = torch.randn(bsz, config.horizon_steps, config.action_dim).to(
             device
-        )  # TODO: training data is normalized within some range?
+        )
         loss = model.loss(
             pixel_values=pixel_values,
             input_ids=input_ids,

src/model/vla/processing.py
@@ -68,11 +68,13 @@ class VLAProcessor:
         tokenizer,
         num_image_tokens: int,
         max_seq_len: int,
+        tokenizer_padding: str = "longest",
     ):
         super().__init__()
 
         self.image_seq_length = num_image_tokens
         self.max_seq_len = max_seq_len
+        self.tokenizer_padding = tokenizer_padding
 
         # Tokenizer described here: https://github.com/google-research/big_vision/blob/main/big_vision/configs/proj/paligemma/README.md#tokenizer
         tokens_to_add = {"additional_special_tokens": [self.IMAGE_TOKEN]}
@@ -95,12 +97,14 @@ class VLAProcessor:
         self,
         text: List[str],
         images: torch.LongTensor,
-        padding: str = "longest",
         truncation: bool = True,
     ) -> dict:
         assert len(images) == len(
             text
         ), f"Received {len(images)} images for {len(text)} prompts."
+        assert (
+            images.dtype == torch.uint8
+        ), f"Expected uint8 tensor for images, got {images.dtype}."
 
         pixel_values = process_images(
             images,
@@ -125,7 +129,7 @@ class VLAProcessor:
             input_strings,
             return_tensors="pt",
             max_length=self.max_seq_len,
-            padding=padding,
+            padding=self.tokenizer_padding,
             truncation=truncation,
         )
 